70999615c69ccbee81d69f7153d19935e20a9cb3
Han <milk88101524@gmail.com>
2024-07-19 15:01:49 +0800

    Initial Commit

---
+++ lauguage_ebook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lauguage_ebook
+//
+//  Created by Han on 2024/7/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
